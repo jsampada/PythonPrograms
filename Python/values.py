7aeceed7ce815141c650d95ae50cb2d8de3a9604
jsampada <joshisampada.03@gmail.com>
2023-01-12 23:46:30 +0530

    Assigning values to variables

---
+++ Python/values.py
@@ -0,0 +1,21 @@
+#assigning one value to one ariable
+name="sampada"
+cname="GHRCEM"
+email="sam@gmail.com"
+mobile=7645368990
+print(name)
+print(cname)
+print(email)
+print(mobile)
+
+#assigning values to multiple variables at a time
+name,cname,email="sai","GHRIT","sai@gmail.com"
+print(name)
+
+#Assignig one value to multiple variables
+hTown=Sloc=Floc=Wloc='hyd'
+print(Sloc)
+
+#Assigning multiple values to one variable
+MyData="sampada joshi","32","GHRCEM","sam@gmail.com"
+print(MyData)
