972688bbe2ed7d65d9148851f1e7d457e7698a11
jsampada <joshisampada.03@gmail.com>
2023-04-01 23:55:22 +0530

    List function program

---
+++ Python/ListOperation.py
@@ -0,0 +1,9 @@
+#list slicing
+amazon_cart =['nootbook','cloth','sunglasses','mobile','laptop','AC']
+#print(amazon_cart[0])
+amazon_cart[0]='computer'
+print(amazon_cart[0])    #we can not update or modify a list we can create a new list or copy of list
+print(amazon_cart[1:3])
+print(amazon_cart)
+
+
