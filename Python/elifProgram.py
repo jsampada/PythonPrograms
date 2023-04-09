bb751242716124ad6599eaf7acb639af7be9de36
jsampada <joshisampada.03@gmail.com>
2023-04-09 22:42:32 +0530

    elif program

---
+++ Python/elifProgram.py
@@ -0,0 +1,31 @@
+
+"""age=25
+if age<=25:
+    print("young")
+else:
+    print("not defined") """
+
+
+#elifprogram
+""""
+Age=int(input("Entere Your Age:"))
+if Age<=18:
+    print("You are adult")
+elif Age>=40:
+    print("you are senior citizen")
+else:
+    print("you are teen")"""
+
+'''is_old ='hello'
+is_licenced = 5
+if is_old and is_licenced:
+    print("hi")
+else:
+    print("bye")'''
+
+
+
+
+
+
+
