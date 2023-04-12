2825b5fd42fa6bca047920cc387445e3f1255843
jsampada <joshisampada.03@gmail.com>
2023-04-12 22:16:45 +0530

    program

---
+++ Python/shortcircuiting.py
@@ -0,0 +1,23 @@
+is_friend=True
+is_user=True
+if is_friend and is_user:
+    print("best friend forver")
+
+
+
+is_friend=True
+is_user=False
+if is_friend or is_user:
+    print("best friend forver")
+
+
+is_friend=False
+is_user=False
+if is_friend or is_user:
+    print("best friend forver")
+
+
+is_friend=False
+is_user=False
+if is_friend and is_user:
+    print("best friend forver")
