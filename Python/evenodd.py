aa3ef595122b4c297102acdcccb4e48dde4079f8
jsampada <joshisampada.03@gmail.com>
2023-07-01 23:57:03 +0530

    program

---
+++ Python/evenodd.py
@@ -0,0 +1,19 @@
+#odd number
+sum=0
+for i in range(1,1000):
+    if i%2!=0:
+       sum=sum+i
+print(sum)
+
+#even number
+sum=0
+for i in range(1,1000):
+    if i%2==0:
+       sum=sum+i
+print(sum)
+
+#addition
+sum=0
+for i in range(1,1000):
+    sum=sum+i
+print(sum)
