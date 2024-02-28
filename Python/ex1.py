15f913ffa5a68b79790c8f60a0c93a61c7374288
jsampada <joshisampada.03@gmail.com>
2024-02-28 19:05:31 +0530

    python program

---
+++ Python/ex1.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+# Input the two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate and print the GCD
+print("The GCD of", num1, "and", num2, "is", gcd(num1, num2))
