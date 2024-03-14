59b069411a138b4f3c47da305871aca4c5cbd59f
jsampada <joshisampada.03@gmail.com>
2024-03-14 20:24:09 +0530

    python program

---
+++ Python/Diamond Pattern.py
@@ -0,0 +1,8 @@
+def diamond_pattern(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+    for i in range(n - 1, 0, -1):
+        print(" " * (n - i) + "* " * i)
+
+# Example usage
+diamond_pattern(5)
