8520e0438cc14c90098d840b7355122c05e3b01e
jsampada <joshisampada.03@gmail.com>
2024-03-18 20:35:28 +0530

    python program

---
+++ Python/Inverted Right Triangle Pattern.py
@@ -0,0 +1,6 @@
+def inverted_right_triangle_pattern(n):
+    for i in range(n, 0, -1):
+        print("* " * i)
+
+# Example usage
+inverted_right_triangle_pattern(5)
---
+++ Python/Left Triangle Pattern.py
@@ -0,0 +1,6 @@
+def left_triangle_pattern(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+
+# Example usage
+left_triangle_pattern(5)
