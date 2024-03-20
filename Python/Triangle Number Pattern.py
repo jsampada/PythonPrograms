1d9f137943cd2e42cf99a97c9154d62e7321e512
jsampada <joshisampada.03@gmail.com>
2024-03-20 15:17:44 +0530

    python program

---
+++ Python/Triangle Number Pattern.py
@@ -0,0 +1,10 @@
+def triangle_number_pattern(n):
+    num = 1
+    for i in range(1, n + 1):
+        for j in range(i):
+            print(num, end=" ")
+            num += 1
+        print()
+
+# Example usage
+triangle_number_pattern(4)
