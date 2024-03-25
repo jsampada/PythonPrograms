f8de296519d6ba6bb19e5caf40c554d1d7fbaf7a
jsampada <joshisampada.03@gmail.com>
2024-03-25 20:26:24 +0530

    python program

---
+++ Python/dict_Check Key Existence.py
@@ -0,0 +1,7 @@
+sample_dict = {'a': 1, 'b': 2, 'c': 3}
+
+key_to_check = 'b'
+if key_to_check in sample_dict:
+    print(f'{key_to_check} exists in the dictionary.')
+else:
+    print(f'{key_to_check} does not exist in the dictionary.')
