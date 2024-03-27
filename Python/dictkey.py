6e5351f3dae193cfaa3bcaeb831521b8162a68ad
jsampada <joshisampada.03@gmail.com>
2024-03-27 15:13:00 +0530

    python program

---
+++ Python/dictkey.py
@@ -0,0 +1,71 @@
+# class a:
+#     def name(self):
+#         print("sam")
+# class b(a):
+#     def name2(self):
+#         print("ram")
+# bc=b()
+# bc.name()
+# bc.name2()
+
+
+#multilevel
+# class a:
+#     def dog(self):
+#         print("hello")
+# class b(a):
+#     def cat(self):
+#         print("this is")
+# class c(b):
+#     def me(self):
+#         print("cat")
+# cs=c()
+# cs.dog()
+# cs.cat()
+# cs.me()
+
+
+
+
+
+
+
+#multipule
+
+
+# class a:
+#     def dog(self):
+#         print("hello")
+# class b:
+#     def cat(self):
+#         print("this is")
+# class c:
+#     def me(self):
+#         print("cat")
+# class d(a,b,c):
+#     def my(self):
+#         print("bye")
+#
+# cs=d()
+# cs.dog()
+# cs.cat()
+# cs.me()
+# cs.my()
+
+
+a=10
+print(type(a))
+b=str(a)
+print(b)
+# print(type(b))
+
+
+str={"key":"sam"}
+print(str)
+
+
+s={1,2,3,4,5}
+s.add(10)
+
+print(s)
+
