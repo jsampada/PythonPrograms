81ac2b09264d6805175d5a132622176a2ac8e92c
jsampada <joshisampada.03@gmail.com>
2024-04-09 10:27:24 +0530

    python program

---
+++ Python/listprac.py
@@ -0,0 +1,14 @@
+#sum of all element in a list
+lst=[1,2,3,4,5,6,7,8,9]
+print(sum(lst))
+print(max(lst))
+print(min(lst))
+
+
+age=eval(input("eneter your age="))
+if age>=18:
+    print("you are allowed to watch a movie")
+else:
+    print("you are not allowed to watch a movie")
+
+
