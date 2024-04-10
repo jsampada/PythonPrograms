e2b378e1a4e452bae88ba8b5a669275b42b26d0b
jsampada <joshisampada.03@gmail.com>
2024-04-10 20:12:51 +0530

    python program

---
+++ Python/classa.py
@@ -0,0 +1,7 @@
+class person:
+    def sam():
+        name = input('enter your name: ')
+        age = int(input('enter your age: '))
+        print('hello,my name is', name, 'and my age is', age)
+    sam()
+person
