a0b09102a39203eb0470d44ab117ccb8af8c21bc
jsampada <joshisampada.03@gmail.com>
2024-04-19 10:32:41 +0530

    python program

---
+++ Python/calculater.py
@@ -0,0 +1,54 @@
+# first_number=eval(input("enter first number : "))
+# second_number=eval(input(("enter second number : ")))
+# print("addition =" ,first_number+second_number)
+# print("subs=",first_number-second_number)
+# print("mul=",first_number*second_number)
+# print("div=",first_number/second_number)
+
+#fruit=['apple','mango','orange','banana']
+# veg=['tomato','potato','chili']
+# # print(fruit,veg)
+# a=[fruit,veg]
+# print(a [1][1])
+# fruit.append('chery')
+# print(fruit)
+# fruit[3]='chery'
+# print(fruit)
+
+
+# for item in range(7,71,7):
+#      print(item)
+
+
+
+# number=eval(input("enter number:"))
+# if number%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# no=32
+# print("your number is",no,",hello")
+# print(f"your number is {no} hello")
+
+
+list=[1,2,3,4,5,6,6,6,6]
+print(list)
+list.pop(3)
+print(list)
+list.extend([7,8,9])
+print(list)
+list.remove(5)
+print(list)
+print(list.count(6))
+print(max(list))
+print(sum(list))
+print(list)
+
+# list.clear()
+# print(list)
+# l=list("1,2,3,4,56,4")
+# print(l)
+
+
+
