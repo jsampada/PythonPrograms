42b43442312eda45c784c75cf6aecba7fc94a9bb
jsampada <joshisampada.03@gmail.com>
2024-04-20 15:58:33 +0530

    python program

---
+++ Python/practice1.py
@@ -0,0 +1,23 @@
+# a='x'
+# b='y'
+# c='z'
+# st=a+b+c
+# print(st)
+
+#string concatination using join operation
+# st1=''.join([a,b,c])
+# print(st1)
+
+
+
+# string1='sampada'
+# print(len(string1))
+# print(string1.count('a'))
+# print(string1.index('d'))
+# print(string1.lower())
+# print(string1.upper())
+# print(string1.split())
+
+
+#creating  list by using []
+#print(lst)
