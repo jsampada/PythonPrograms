c411a284e52c59645aa52bcfbb7670f85ab88565
jsampada <joshisampada.03@gmail.com>
2024-05-07 23:16:26 +0530

    python program

--- Python/ass.prefix.py
+++ Python/ass.prefix.py
@@ -1,6 +1,11 @@
 a=[1,2,3,4,5,6,7,8,9]
-#
+s=[]
+for i in a:
+    s1=str(i)+"x"
+    s.append(s1)
+print(s)
+
 # str1=[str(i)+"x" for i in a]
 # print(str1)
-s=a[1::2]
-print(s)
+# s=a[1::2]
+# print(s)
