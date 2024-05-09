61cc43a7fe49462afc2b69d219b5ef9a7cceb1af
jsampada <joshisampada.03@gmail.com>
2024-05-09 21:59:33 +0530

    python program

---
+++ Python/with ip with returntype.py
@@ -0,0 +1,14 @@
+class calculater:
+    def __init__(self):
+        self.x=100
+        self.y=200
+    def add(self,a,b):
+        c=a+b
+        return c
+c1=calculater()
+print(c1.x)
+print(c1.y)
+a=10
+b=20
+result=c1.add(a,b)
+print(result)
