eca91139f5cfe059b5dfc77411aa4664be639f14
jsampada <joshisampada.03@gmail.com>
2024-05-09 21:59:42 +0530

    python program

---
+++ Python/with_iptype_without_return type.py
@@ -0,0 +1,12 @@
+class calculater:
+    def __init__(self):
+        self.x=100
+        self.y=200
+    def add(self,a,b):
+        c=a+b
+c1=calculater()
+print(c1.x)
+print(c1.y)
+a=10
+b=20
+c1.add(a,b)
