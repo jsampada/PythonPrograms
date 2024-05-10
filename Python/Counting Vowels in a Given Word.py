9fedb616401262d92c4e779892356321b7f3689a
jsampada <joshisampada.03@gmail.com>
2024-05-10 22:05:31 +0530

    python program

--- Python/Counting Vowels in a Given Word.py
+++ Python/Counting Vowels in a Given Word.py
@@ -1,8 +1,8 @@
 vowel = ['a', 'e', 'i', 'o', 'u']
 word = "programming"
 count = 0
-for character in word:
-    if character in vowel:
+for i in word:
+    if i in vowel:
         count += 1
 print(count)
 
