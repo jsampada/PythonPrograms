083265dd040bc9bdaeb4a60c55672d121d026d3d
jsampada <joshisampada.03@gmail.com>
2024-05-12 19:48:39 +0530

    python program

---
+++ Python/class-object.py
@@ -0,0 +1,9 @@
+class fan:
+    def __init__(self):
+        self.brand='USHA'
+        self.price=5000
+        print(self)
+    def rotate(self):
+        print("fan is rotating")
+f=fan()
+print(f)
