064d0b69b957deef32ea5aeffc3c82d4cd8b6550
jsampada <joshisampada.03@gmail.com>
2024-05-13 23:01:26 +0530

    python program

---
+++ Python/standard way of writting a class.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,a,b):
+        self.name=a
+        self.age=b
+    def print_name(self):
+        pass
+s1=student("sampada",21)
+s2=student("sita",23)
+s3=student("gita",24)
+print(s1.name)
+print(s2.name)
+print(s3.name)
