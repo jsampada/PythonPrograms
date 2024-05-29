ec3c03ab28e46e34f01e0401adfa6a2c1bc7c2c7
jsampada <joshisampada.03@gmail.com>
2024-05-29 19:25:39 +0530

    python program

---
+++ Python/fun overrriding.py
@@ -0,0 +1,7 @@
+def fun():
+    print("function 1")
+def fun():
+    print("fun 2")
+def fun():
+    print("fun 3")
+fun()
---
+++ Python/funoverloading.py
@@ -0,0 +1,8 @@
+def fun1(a):
+    print("1 parameter")
+def fun1(a,b):
+    print("2 para")
+def fun1(a,b,c):
+    print("3 para")
+
+fun1(20,10,30)
