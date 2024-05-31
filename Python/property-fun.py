19a99a15a5b412d77e834df1482d39756e37f6c4
jsampada <joshisampada.03@gmail.com>
2024-05-31 18:33:17 +0530

    python program

---
+++ Python/property-fun.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self):
+        self.__name=''
+    def getter(self):
+        return self.__name
+    def setter(self,value):
+        self.__name=value
+    getset=property(getter,setter)
+s1=Student
+s1.getset='sampada'
+res=s1.getset
+print(res)
